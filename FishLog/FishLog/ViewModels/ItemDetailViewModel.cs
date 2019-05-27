9b423045b1e321a2d290d3dd3345e16daf9ed3b9
Christopher Wilson <wilsoc13@ferris.edu>
2019-05-26 22:00:29 -0400

    Add project files.

---
+++ FishLog/FishLog/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FishLog.Models;
+
+namespace FishLog.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
